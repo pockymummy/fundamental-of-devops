156382144b165f6552c0d5c54128d62875ffdf93
Thanawat <thanawat.tangpornpisit@gmail.com>
2026-02-08 11:22:00 +0700

    kubernetes local

--- ch3/docker/app.js
+++ ch3/docker/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Fundamentals of DevOps!\n');
+	res.end('Hello World!\n');
 });
 
 const port = process.env.PORT || 8080;
